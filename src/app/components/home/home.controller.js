422afd88d1f73386283112d9211c11452287bc6c
jtomchak <jtomchak@gmail.com>
2017-06-08 15:44:22 -0700

    let's make a detailed page

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -10,13 +10,19 @@ class HomeController {
     this.PostService = PostService;
     this.getPosts();
   }
- //TODO: let's make posts show in the view
- //BONOUS: only the first 10 
+
   getPosts() {
     this.PostService.get()
       .then(() => {
         this.posts = this.PostService.getState();
       });
+  }
+
+//TODO: this method is called when a user selects a post from the homepage 
+//TODO: what we want is for them to be taken to a 'details' page with all the info from that post
+//TODO BONOUS: back button on page, not browser back to take them back to the full list
+  onSelect(postId) {
+    console.log(postId);
   }
 }
 
