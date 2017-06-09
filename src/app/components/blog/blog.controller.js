e43b788dcc3da6c7ad7a9388ed7c66d4711ca7ea
jtomchak <jtomchak@gmail.com>
2017-06-08 17:05:59 -0700

    made me some detail and moved the posts to the blog page

--- src/app/components/blog/blog.controller.js
+++ src/app/components/blog/blog.controller.js
@@ -1,10 +1,28 @@
 import _ from 'lodash';
 
-export class BlogController {
-  constructor() {
-    this.message = 'The latest from the blog';
+class BlogController {
+  constructor(PostService, $location) {
+    this.message = 'The latest from the blog!';
+    this.PostService = PostService;
+    this.$location = $location;
+    this.getPosts();
+  }
 
+  getPosts() {
+    this.PostService.get()
+      .then(() => {
+        this.posts = this.PostService.getState();
+      });
+  }
+
+//TODO: this method is called when a user selects a post from the homepage 
+//TODO: what we want is for them to be taken to a 'details' page with all the info from that post
+//TODO BONOUS: back button on page, not browser back to take them back to the full list
+  onSelect(postId) {
+    this.$location.path('details/' + postId.toString());//Go to location!
   }
 }
 
-
+BlogController.$inject = ['PostService', '$location'];
+// could also just export the class up top as well
+export {BlogController};
--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -5,27 +5,10 @@ import _ from 'lodash';
 class HomeController {
   // bind to this and not $scope
   // because of controllerAs.
-  constructor(PostService) {
+  constructor() {
     this.title = 'Welcome to the blog!';
-    this.PostService = PostService;
-    this.getPosts();
-  }
-
-  getPosts() {
-    this.PostService.get()
-      .then(() => {
-        this.posts = this.PostService.getState();
-      });
-  }
-
-//TODO: this method is called when a user selects a post from the homepage 
-//TODO: what we want is for them to be taken to a 'details' page with all the info from that post
-//TODO BONOUS: back button on page, not browser back to take them back to the full list
-  onSelect(postId) {
-    console.log(postId);
   }
 }
 
-HomeController.$inject = ['PostService'];
 // could also just export the class up top as well
 export {HomeController};
